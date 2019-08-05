d72c15d524a3e4e764e1d2b6dbafd556a61cf8d2
PeerChristensen <hr.pchristensen@gmail.com>
2019-08-05 23:30:12 +0200

    check updates

---
+++ checking_updates.R
@@ -0,0 +1,5 @@
+# script for checking updates
+
+library(tidyverse)
+df <- read_csv(paste0("deltas/",list.files("deltas")[length(list.files("deltas"))]))
+df %>% filter(seneste_kontrol == 4) %>% select(By,navn1)
