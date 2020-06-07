c612f49d48073dbfd7b9bbc43747f6a98aff4017
PeerChristensen <hr.pchristensen@gmail.com>
2020-06-07 08:33:36 +0200

    app2

---
+++ smile/app.R
@@ -0,0 +1,139 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(leaflet)
+library(shinythemes)
+
+data <- read_csv("smiley_data.csv")
+
+# Define UI
+ui <- shinyUI(fluidPage(theme = shinytheme("cerulean"),
+    # Application title
+    titlePanel("Seneste Fødevarekontroller"),
+    
+    # navbar
+    navbarPage("",
+    
+    #panel 1
+    tabPanel("Kort",
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(width = 3,
+                     tags$head( tags$style( type = "text/css", '
+      .irs-line-mid{
+        background: #428bca ;
+        border: 1px solid #428bca ;
+      }
+      .irs-line-right{
+        background: #428bca ;
+      }
+      .irs-bar {
+        background: linear-gradient(to bottom, #DDD -50%, #FFF 150%);
+        border-top: 1px solid #CCC ;
+        border-bottom: 1px solid #CCC ;
+      }
+      .irs-bar-edge {
+        background: inherit ;
+        border: inherit ;
+      }
+
+    ')), 
+            sliderInput("dato",
+                        "Fra dato:",
+                        min = min(data$seneste_kontrol_dato),
+                        max = max(data$seneste_kontrol_dato),
+                        value = lubridate::today()-180),
+
+            checkboxGroupInput("checkGroup", 
+                                      "Smiley/karakter", 
+                                      choices = list("1 (Glad)" = 1, 
+                                                     "2" = 2, 
+                                                     "3" = 3,
+                                                     "4 (Sur)" = 4),
+                                      selected = c(1,2,3,4)))
+        ,
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            leafletOutput("map",width = "110%", height = 800)
+        ) # main panel
+        
+        ) # sidebar layour
+    ), # tab panel 1
+    
+    tabPanel("Data",align="center",
+             downloadButton('download',"Download .csv"),
+             
+             DT::dataTableOutput("dataframe",width = "80%"))
+    ) # navbar
+    ) # UI
+    )
+    
+
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    
+    # MAP #########################
+    # filter data based on input from ui
+    map_data_react <- reactive({
+        
+        data %>%
+            filter(seneste_kontrol_dato >= input$dato,
+                   seneste_kontrol %in% input$checkGroup)
+        
+    })
+
+    output$map <- renderLeaflet({
+        
+        data <- map_data_react()  # Add this
+        
+        leaflet() %>%
+            addProviderTiles("CartoDB.Positron", group = "CartoDB.Positron") %>%
+            addCircleMarkers(data = data, opacity = .5,fillOpacity = .8,radius=5, stroke = T, col = ~colour,
+                             label = ~navn1,
+                             popup = paste(
+                                 data$navn1,"<br>",
+                                 "CVR: ", data$cvrnr, "<br>",
+                                 data$adresse1,"<br>",
+                                 data$postnr,data$By, "<br>",
+                                 "Seneste kontrol: ",data$seneste_kontrol_dato, "<br>",
+                                 "<a href=",data$URL,'> Rapport</a>'))
+        #,clusterOptions = markerClusterOptions())
+    })
+    
+    # DATA #############################
+    output$dataframe <- DT::renderDataTable({
+         
+        #DT::dataTableAjax(session, data, outputId = "dataframe")
+        data <- data %>%
+            select(navn1:URL) %>%
+            rename("Navn" = "navn1","CVR" = "cvrnr", "Adresse" = "adresse1",
+                   "Postnr" = "postnr", "Seneste smiley/karakter" = "seneste_kontrol",
+                   "Dato seneste kontrol" = "seneste_kontrol_dato","Rapport" = "URL") %>%
+            mutate(Rapport = paste("<a href=",data$URL,'> Link </a>'))
+        
+        DT::datatable(data, escape = FALSE)
+    })
+    
+    output$download <- 
+        downloadHandler(
+            filename = "data.csv",
+            content = function(file){
+                write.csv(data,
+                          file) }
+    )
+}
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
